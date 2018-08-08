093c778c903b5a157fd448a61717175da544a0e6
Niz97 <97niran@gmail.com>
2018-08-08 18:56:06 +0100

    Basic printing of given number of headlines
    Implemented the basic code to print out a specific number of article headlines.

--- scraper.py
+++ scraper.py
@@ -12,6 +12,13 @@ articles = soup.select(".gs-c-promo-heading__title")
 
 # print("articles contains {} elements\n".format(len(articles)))
 
-for article in articles:
-	print(article.get_text())
+# for article in articles:
+# 	print(article.get_text())
 	
+def print_element_text(list_of_elements, n):
+	number_of_elements = n
+	elements = list_of_elements
+	for i in range(number_of_elements):
+		print(elements[i].get_text())
+
+print_element_text(articles, 6)
